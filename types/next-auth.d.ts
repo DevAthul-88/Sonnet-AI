2e30bfdef4f1f39c69367cec41d6cbd553147148
DevAthul-88 <devathulvinod@gmail.com>
2024-11-11 09:02:34 -0800

    fixed

--- types/next-auth.d.ts
+++ types/next-auth.d.ts
@@ -1,16 +1,6 @@
-import { UserRole } from "@prisma/client";
 import { User } from "next-auth";
 import { JWT } from "next-auth/jwt";
 
-export type ExtendedUser = User & {
-  role: UserRole;
-};
-
-declare module "next-auth/jwt" {
-  interface JWT {
-    role: UserRole;
-  }
-}
 
 declare module "next-auth" {
   interface Session {
