5dd7b408433732ad384d6abb094a697c74b93c2f
DevAthul-88 <devathulvinod@gmail.com>
2024-11-11 08:56:13 -0800

    fixed

--- auth.ts
+++ auth.ts
@@ -1,19 +1,10 @@
 import authConfig from "@/auth.config";
 import { PrismaAdapter } from "@auth/prisma-adapter";
-import { UserRole } from "@prisma/client";
 import NextAuth, { type DefaultSession } from "next-auth";
 
 import { prisma } from "@/lib/db";
 import { getUserById } from "@/lib/user";
 
-// More info: https://authjs.dev/getting-started/typescript#module-augmentation
-declare module "next-auth" {
-  interface Session {
-    user: {
-      role: UserRole;
-    } & DefaultSession["user"];
-  }
-}
 
 export const {
   handlers: { GET, POST },
